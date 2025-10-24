e710d5e1832b770f7480b3b365252cd6842979ae
Hu <146780983+HutaroZ12@users.noreply.github.com>
2025-10-24 01:56:21 -0300

    Update PlayState.hx

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -623,7 +623,7 @@ class PlayState extends MusicBeatState
 		// SONG SPECIFIC SCRIPTS
 		#if (LUA_ALLOWED || HSCRIPT_ALLOWED)
 		for (folder in Mods.directoriesWithFile(Paths.getSharedPath(), 'data/$songName/'))
-		for (event in Mods.directoriesWithFile(Paths.getSharedPath(), 'custom_events/'))
+		for (event in Paths.getSharedPath(), 'custom_events/' + event + '.lua'))
 			#if linux
 			for (file in CoolUtil.sortAlphabetically(NativeFileSystem.readDirectory(folder)))
 			#else
